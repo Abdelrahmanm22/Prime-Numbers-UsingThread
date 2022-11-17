e230792588b729cabcb8b0e46d2999b77f706e88
AbdelrahmanTelabrak <73077461+AbdelrahmanTelabrak@users.noreply.github.com>
2022-11-17 22:30:58 +0200

    Update Primes.java

--- primes/src/primes/Primes.java
+++ primes/src/primes/Primes.java
@@ -11,31 +11,32 @@ public class Primes {
 		public static void main(String[] args) throws IOException, InterruptedException {
 	        // TODO code application logic here
 	        
-	       Scanner input = new Scanner(System.in);
-	       System.out.print("Ente N: ");
-	       int N=input.nextInt();
-	       PrintStream console = System.out;
-	       buffer buf = new buffer(N);
-	       File file = new File("C:\\Users\\Ahmed\\Destop");
-	       PrintStream stream = new PrintStream(file);
-	      
-	       
-	       
-	        Producer P = new Producer(buf);
-	        Consumer c = new Consumer(buf , stream);
-	        
-	        P.start();
-	        c.start();
-	        
-	        
-	        
-	        P.join();
-	        c.join();
-	        
-	        System.setOut(console);
-	        
-	        
-	        System.out.println("The Max Prime is " + buf.Max_prime+"\n"+ "The Numbers of Primes are " + buf.N);
+			new Frame();
+//	       Scanner input = new Scanner(System.in);
+//	       System.out.print("Ente N: ");
+//	       int N=input.nextInt();
+//	       PrintStream console = System.out;
+//	       buffer buf = new buffer(N);
+//	       File file = new File("D:\\Os\\output.txt");
+//	       PrintStream stream = new PrintStream(file);
+//	      
+//	       
+//	       
+//	        Producer P = new Producer(buf);
+//	        Consumer c = new Consumer(buf , stream);
+//	        
+//	        P.start();
+//	        c.start();
+//	        
+//	        
+//	        
+//	        P.join();
+//	        c.join();
+//	        
+//	        System.setOut(console);
+//	        
+//	        
+//	        System.out.println("The Max Prime is " + buf.Max_prime+"\n"+ "The Numbers of Primes are " + buf.N);
 	    }
 	        
 	        
